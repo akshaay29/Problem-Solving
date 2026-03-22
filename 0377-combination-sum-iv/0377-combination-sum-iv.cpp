cd7f0d31d890d67e59f19a1598dc98bb573dc05b
Akshay Gupta <guptaakshay798@gmail.com>
2026-03-23 04:01:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int solve(vector<int>&dp,vector<int>&nums,int target){
-        if(target==0) return 1;
-        if(target<0) return 0;
-        if(dp[target]!=-1) return dp[target];
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            ans+=solve(dp,nums,target-nums[i]);
+        dp[0]=1;
+        for(int i=0;i<=target;i++){
+            for(int j=0;j<nums.size();j++){
+                if(i-nums[j]>=0) dp[i]+=dp[i-nums[j]];
+            }
         }
-        return dp[target]=ans;
+        return dp[target];
     }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int>dp(target+1,-1);
+        vector<int>dp(target+1,0);
         return solve(dp,nums,target);
     }
 };
