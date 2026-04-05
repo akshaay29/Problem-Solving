7bb0b5f6699a3732deba083763a38b2d35e847fc
Akshay Gupta <guptaakshay798@gmail.com>
2026-04-05 18:52:25 +0530

    Time: 8 ms (47.41%), Space: 14.4 MB (15.94%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distanceMemoization.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
+        if(i==s1.size()) return s2.size()-j;
+        if(j==s2.size()) return s1.size()-i;
+        if(s1[i]==s2[j] ) return 0+solve(i+1,j+1,s1,s2,dp);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ins=1+solve(i,j+1,s1,s2,dp);
+        int del=1+solve(i+1,j,s1,s2,dp);
+        int rep=1+solve(i+1,j+1,s1,s2,dp);
+        return dp[i][j]=min({ins,del,rep});
+    }
+    int minDistance(string s1, string s2) {
+        int i=0 , j=0 , n=s1.size() , m=s2.size();
+        vector<vector<int>>dp(n+1 , vector<int>(m+1,-1));
+        return solve(i,j,s1,s2,dp);
+    }
+};
