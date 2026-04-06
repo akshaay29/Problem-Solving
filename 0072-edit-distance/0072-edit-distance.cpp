cc5a1f8c3402666d2761a6b8e8d0ed8194213eb2
Akshay Gupta <guptaakshay798@gmail.com>
2026-04-06 23:57:51 +0530

    Time: 7 ms (63.35%), Space: 13.4 MB (29.19%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
+    /*int solve(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
         if(i==s1.size()) return s2.size()-j;
         if(j==s2.size()) return s1.size()-i;
         if(s1[i]==s2[j] ) return 0+solve(i+1,j+1,s1,s2,dp);
@@ -9,10 +9,21 @@ public:
         int del=1+solve(i+1,j,s1,s2,dp);
         int rep=1+solve(i+1,j+1,s1,s2,dp);
         return dp[i][j]=min({ins,del,rep});
-    }
+    }*/
     int minDistance(string s1, string s2) {
-        int i=0 , j=0 , n=s1.size() , m=s2.size();
-        vector<vector<int>>dp(n+1 , vector<int>(m+1,-1));
-        return solve(i,j,s1,s2,dp);
+        int n=s1.size() , m=s2.size();
+        vector<vector<int>>dp(n+1 , vector<int>(m+1,0));
+        for(int i=0;i<n;i++) dp[i][m]=n-i;
+        for(int j=0;j<m;j++) dp[n][j]=m-j;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(s1[i]==s2[j]) dp[i][j]=0 + dp[i+1][j+1];
+                else {
+                    int ins=1+ dp[i][j+1] , del=1 + dp[i+1][j] , rep=1 + dp[i+1][j+1];
+                    dp[i][j]=min({ins,del,rep});
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
