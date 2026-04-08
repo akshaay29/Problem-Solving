7fa82668ecaea0d99bbef4de22925468d8298900
Akshay Gupta <guptaakshay798@gmail.com>
2026-04-08 15:29:54 +0530

    Time: 0 ms (100%), Space: 16.3 MB (39.55%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sumTabulation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size() , n=grid[0].size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,INT_MAX));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1) dp[i][j]=grid[i][j];
+                else dp[i][j]=grid[i][j]+min(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
+};
