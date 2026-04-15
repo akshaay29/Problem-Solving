dc938b6e8baf49823690dfe96c59fdd64d803457
Akshay Gupta <guptaakshay798@gmail.com>
2026-04-16 05:01:45 +0530

    Time: 357 ms (24.16%), Space: 0B (100%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT e.employee_id
+FROM Employees e
+WHERE e.salary<30000 AND e.manager_id NOT IN (SELECT m.employee_id
+                                                FROM employees m)
+ORDER BY e.employee_id;
