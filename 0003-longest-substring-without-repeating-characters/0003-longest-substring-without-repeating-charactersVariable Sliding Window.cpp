ae9ea96ab4e7c48b66a733303f05fcb3e6edaf50
Akshay Gupta <guptaakshay798@gmail.com>
2026-07-13 15:11:20 +0530

    Time: 12 ms (41.56%), Space: 14.2 MB (27.74%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-charactersVariable Sliding Window.cpp
@@ -0,0 +1,19 @@
+using ll=long long;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int left=0 , cnt=0 , maxCnt=0;
+        unordered_set<int>st;
+        for(int right=0;right<s.size();right++){
+            cnt+=1;
+            st.insert(s[right]);
+            while(st.size()!=cnt){
+                cnt-=1;
+                st.erase(s[left++]);
+                st.insert(s[right]);
+            }
+            maxCnt=max(maxCnt,cnt);
+        }
+        return maxCnt;
+    }
+};
