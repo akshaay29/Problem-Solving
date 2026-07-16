2f2bc37455cb8e02dd7aa164595f554b09b2ce7a
Akshay Gupta <guptaakshay798@gmail.com>
2026-07-16 12:40:33 +0530

    Time: 3 ms (46.69%), Space: 24.2 MB (25.1%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutablePrefix Sum.cpp
@@ -0,0 +1,24 @@
+class NumArray {
+public:
+    vector<int>prefixSum;
+    NumArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int n=nums.size();
+        prefixSum.resize(n+1);
+        prefixSum[0]=0;
+        for(int i=1;i<=nums.size();i++){
+            prefixSum[i]=prefixSum[i-1]+nums[i-1];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return prefixSum[right+1]-prefixSum[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
