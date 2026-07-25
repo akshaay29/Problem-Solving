9176e2608848e17208cec659f1c0cad66bce4990
Akshay Gupta <guptaakshay798@gmail.com>
2026-07-26 03:10:59 +0530

    Time: 195 ms (5%), Space: 93.4 MB (5.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -12,9 +12,18 @@ public:
     int coinChange(vector<int>& coins, int amt) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        int n=coins.size();
-        vector<vector<int>>dp(amt+1 ,vector<int>(n,-1));
-        int res=solve(0,amt,coins,dp);
-        return (res==INT_MAX) ? -1:res;
+        int n=coins.size() ;
+        vector<vector<int>>dp(amt+1 ,vector<int>(n,0));
+        for(int j=0;j<n;j++) dp[0][j]=0;
+        for(int i=1;i<=amt;i++){
+            for(int j=n-1;j>=0;j--){
+                dp[i][j]=1e9;
+                int inc=1e9 , exc=1e9;
+                if(i>=coins[j]) inc=1+dp[i-coins[j]][j];
+                if(j+1<n) exc=dp[i][j+1];
+                dp[i][j]=min(inc , exc);
+            }
+        }
+        return dp[amt][0]>=1e9 ? -1:dp[amt][0];
     }
 };
