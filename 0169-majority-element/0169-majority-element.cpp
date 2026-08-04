bcf8ef16b479891755c36fbda88b6fcd33b77322
Akshay Gupta <guptaakshay798@gmail.com>
2026-08-04 18:26:59 +0530

    Time: 0 ms (100%), Space: 28.3 MB (13.06%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt=0 , ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(cnt==0) ans=nums[i];
+            if(ans==nums[i]) cnt+=1;
+            else cnt-=1;
+        }
+        return ans;
+    }
+};
