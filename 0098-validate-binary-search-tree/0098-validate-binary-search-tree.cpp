dc26bbd2bb08df4ddc58497b183be00535423b0d
Akshay Gupta <guptaakshay798@gmail.com>
2026-08-17 04:05:36 +0530

    Time: 0 ms (100%), Space: 22 MB (16.06%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,21 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
+using ll=long long;
 class Solution {
 public:
-    bool check(TreeNode* root,long long high,long long low){
-        if(root==NULL) return true;
-        if(root->val<low || root->val>high) return false;
-        bool leftSub=check(root->left,root->val,low);
-        bool rightSub=check(root->right,high,root->val);
-        if(leftSub && rightSub && (root->val<high && root->val>low)) return true;
-        return false;
+    bool solve(TreeNode* root,ll high,ll low){
+        if(!root) return true;
+        if(root->val<=low || root->val>=high) return false;
+        bool lsub=solve(root->left,root->val,low);
+        bool rsub=solve(root->right,high,root->val);
+        return (lsub && rsub) ? true:false;
     }
     bool isValidBST(TreeNode* root) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        long long high=LLONG_MAX,low=LLONG_MIN;
-        return check(root,high,low);
+        ll high=LLONG_MAX , low=LLONG_MIN;
+        return solve(root,high,low); 
     }
 };
