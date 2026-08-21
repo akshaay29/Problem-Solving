fd0ab1be12c421a26aed3ae5e74514fb3c7a0307
Akshay Gupta <guptaakshay798@gmail.com>
2026-08-22 03:22:35 +0530

    Time: 2 ms (5.05%), Space: 8.7 MB (20.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    int solve(int n,vector<int>&dp){
+        if(n==0) return 1;
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int one=solve(n-1,dp);
+        int two=solve(n-2,dp);
+        return dp[n]=(one+two);
+    }
     int climbStairs(int n) {
-        if(n<=1) return 1;
-        int prev1=1,prev2=1,curr=0;
-        for(int i=2;i<=n;i++){
-            curr=prev1+prev2;
-            prev2=prev1;
-            prev1=curr;
-        }
-        return curr;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
     }
 };
