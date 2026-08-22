b177a5e51bdacdb7e93a1b695cb69d41090fed8f
Akshay Gupta <guptaakshay798@gmail.com>
2026-08-22 16:42:20 +0530

    Time: 0 ms (100%), Space: 14.1 MB (87.44%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         vector<int>temp;
         temp.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++){
-            if(temp.back()<nums[i]) temp.push_back(nums[i]);;
-            if(temp.back()>=nums[i]){
+        for(int i=0;i<(int)nums.size();i++){
+            if(temp.back()<nums[i]) temp.push_back(nums[i]);
+            else{
                 int lb=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
                 temp[lb]=nums[i];
             }
