69fb95fdd7ee60383c68dce767c5f3513a689bcd
Akshay Gupta <guptaakshay798@gmail.com>
2026-08-28 16:57:30 +0530

    Time: 0 ms (100%), Space: 8.5 MB (7.58%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&dp){
+    /*int solve(int n,vector<int>&dp){
         if(n<=1) return n;
         if(n==2) return 1;
         if(dp[n]!=-1) return dp[n];
         return dp[n]=solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
-    }
+    }*/
     int tribonacci(int n) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        /*vector<int>dp(n+1,-1);
+        return solve(n,dp);*/
+        vector<int>dp(n+1,1);
+        dp[0]=0;
+        for(int i=3;i<=n;i++) dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        return dp[n];
     }
 };
