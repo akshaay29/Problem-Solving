05abca5d637f4f26f8ea2319625458cc9e263890
Akshay Gupta <guptaakshay798@gmail.com>
2026-08-28 16:36:13 +0530

    Time: 0 ms (100%), Space: 8 MB (10.34%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        if(n%2 == 0) return true;
+        return false;
+    }
+};
