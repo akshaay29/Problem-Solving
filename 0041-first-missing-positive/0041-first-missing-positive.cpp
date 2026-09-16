914f8754c5dafec35712fc3edcb96a76aa683f40
Akshay Gupta <guptaakshay798@gmail.com>
2026-09-17 04:33:23 +0530

    Time: 0 ms (100%), Space: 54.7 MB (99.87%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,12 +4,19 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         int n=nums.size();
-        vector<int>res(n+2,-1);
+        for(int i=0;i<n;i++) if(nums[i]<=0) nums[i]=n+1;
+        for(int i=0;i<n;i++){
+            int j=abs(nums[i]);
+            if(j>=1 && j<=n) nums[j-1]=-abs(nums[j-1]);
+        }
+        for(int i=0;i<n;i++) if(nums[i]>0) return i+1;
+        return n+1;
+        /**vector<int>res(n+2,-1);
         for(int i=0;i<n;i++){
             int j=nums[i];
             if(j>=0 && j<=n) res[j]=1;
         }
         for(int i=1;i<=n+1;i++) if(res[i]==-1) return i;
-        return 0;
+        return 0;**/
     }
 };
