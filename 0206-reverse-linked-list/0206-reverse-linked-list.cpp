ab41740af2850e0cf407bd9e3aa73867d597b181
Akshay Gupta <guptaakshay798@gmail.com>
2026-09-20 02:42:09 +0530

    Time: 0 ms (100%), Space: 13.8 MB (7.65%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,19 +8,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ /* struct Listnode{
+        int val;
+        ListNode* next;
+        ListNode() :  val(0),next(nullptr)  {}
+        ListNode(int x) :  val(x),next(nullptr) {}
+        ListNode(int x,ListNode* next) :val(x),next(next){}
+    }
+ */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
+        if(head==NULL) return head;
         ListNode* curr=head;
+        ListNode* forw=NULL;
         ListNode* prev=NULL;
-        ListNode* forward=NULL;
         while(curr!=NULL){
-            forward=curr->next;
+            forw=curr->next;
             curr->next=prev;
             prev=curr;
-            curr=forward;
+            curr=forw;
         }
         return prev;
     }
