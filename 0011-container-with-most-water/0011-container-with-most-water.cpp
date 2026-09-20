6815d778f6a355c92fc1811bb6d45b0642a59d47
Akshay Gupta <guptaakshay798@gmail.com>
2026-09-20 15:29:36 +0530

    Time: 0 ms (100%), Space: 63.6 MB (10.92%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int i=0,j=height.size()-1 , max_area=0;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int i=0 , j=height.size()-1 ,maxArea=INT_MIN;
         while(i<j){
-            int w=j-i , h=min(height[i],height[j]);
-            int area=w*h;
-            max_area=max(area,max_area);
+            maxArea=max(maxArea,min(height[i],height[j]) * (j-i));
             if(height[i]<height[j]) i+=1;
             else j-=1;
         }
-        return max_area;
+        return maxArea;
     }
 };
